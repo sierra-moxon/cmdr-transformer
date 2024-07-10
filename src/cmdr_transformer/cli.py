8afd4a1dcdae1d09d81744e9783214a955d2127d
Sierra Taylor Moxon <sierra.taylor@gmail.com>
2024-07-10 10:53:10 -0700

    initializing repo

---
+++ src/cmdr_transformer/cli.py
@@ -0,0 +1,44 @@
+"""Command line interface for cmdr-transformer."""
+import logging
+
+import click
+
+from cmdr_transformer import __version__
+from cmdr_transformer.main import demo
+
+__all__ = [
+    "main",
+]
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+@click.version_option(__version__)
+def main(verbose: int, quiet: bool):
+    """
+    CLI for cmdr-transformer.
+
+    :param verbose: Verbosity while running.
+    :param quiet: Boolean to be quiet or verbose.
+    """
+    if verbose >= 2:
+        logger.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    else:
+        logger.setLevel(level=logging.WARNING)
+    if quiet:
+        logger.setLevel(level=logging.ERROR)
+
+
+@main.command()
+def run():
+    """Run the cmdr-transformer's demo command."""
+    demo()
+
+
+if __name__ == "__main__":
+    main()
